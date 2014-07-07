05e3501313da7a4a3bee0fc520a7d8addb7f3efe
梅晓华 <i@zivn.me>
2014-07-07 18:05:45 +0800

    更新过滤词检查

--- lua/core/util/string.lua
+++ lua/core/util/string.lua
@@ -157,7 +157,7 @@ function String:replaceFilter(str)
                 break
             end
 
-            if node[0] then
+            if node[-1] then
                 invalid = true
                 break
             end
